33ec522c283cb97935d12ccf93cd3ce9c7253452
Sergey Belevkin <box5551@yandex.ru>
2023-05-04 03:57:55 +0300

    ПР14.1

--- routes/cards.js
+++ routes/cards.js
@@ -8,6 +8,18 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
+cardsRouter.get('/', getAllCards);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/^https?:\/\/(?:[\w-]+\.)+[a-z]{2,}(?:\/\S*)?$/i),
+  }),
+}), createCard);
+cardsRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), deleteCard);
 cardsRouter.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
@@ -18,17 +30,5 @@ cardsRouter.delete('/:cardId/likes', celebrate({
     cardId: Joi.string().length(24).hex(),
   }),
 }), dislikeCard);
-cardsRouter.delete('/:cardId', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex(),
-  }),
-}), deleteCard);
-cardsRouter.get('/', getAllCards);
-cardsRouter.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regex(/^https?:\/\/(?:[\w-]+\.)+[a-z]{2,}(?:\/\S*)?$/i),
-  }),
-}), createCard);
 
 module.exports = cardsRouter;
